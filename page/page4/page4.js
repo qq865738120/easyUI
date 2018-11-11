e4953367744074529ea1ac2847ef6c2b29c83eb2
qq865738120 <zwj865738120@163.com>
2018-11-11 18:07:50 +0800

    v1.0.4

--- page/page4/page4.js
+++ page/page4/page4.js
@@ -5,7 +5,76 @@ const code = {
   title='编辑名片'
   subTitle='2018-09-27 11:41:48'>
 </e-base-list>`,
-
+  d2: `<e-icon-list
+  title='小姐姐'
+  subTitle='13066666666'
+  detail='2018-06-01 13:12'>
+  <e-head-portrait
+    slot='icon'
+    size='150'
+    src='/imgs/default_head.jpg'>
+  </e-head-portrait>
+  <e-enhance-icon
+    slot='right'
+    width='20'
+    height='40'
+    src='/imgs/back.png'>
+  </e-enhance-icon>
+</e-icon-list>`,
+  d3: `<e-icon-list
+  title='系统设置'
+  lineColor='1px solid #e0e0e0'>
+  <e-enhance-icon
+    style='height: 48rpx;'
+    width='48'
+    height='48'
+    slot='icon'
+    src='/imgs/icon.png'>
+  </e-enhance-icon>
+  <e-enhance-view
+    slot='right'
+    type='endCenter'>
+    <text style='color: #909090; font-size: 28rpx; margin-right: 12rpx;'>版本V1.1.2</text>
+    <e-enhance-icon
+      slot='right'
+      width='15'
+      height='30'
+      src='/imgs/back.png'>
+    </e-enhance-icon>
+  </e-enhance-view>
+</e-icon-list>`,
+  d4: `<e-icon-list
+  title='小嘎子'
+  subTitle='16666669999'
+  detail=''>
+  <e-head-portrait
+    slot='icon'
+    src='/imgs/default_head.jpg'
+    textColor='#666'
+    spacing='20'
+    size='150'>
+    <e-base-button
+      text='我的好友' 
+      theme='filling'
+      width='120'
+      height='36'
+      textSize='20'
+      color='#FE9036'
+      radius='30'
+      bindbuttontap='testTap'></e-base-button>
+  </e-head-portrait>
+  <e-base-button
+    slot='right'
+    text='金牌会员' 
+    theme='fillingAndGradient2'
+    width='155'
+    height='50'
+    textSize='24'
+    color='#FE9036'
+    endColor='#FE4E36'
+    radius='50'>
+  </e-base-button>
+</e-icon-list>`,
 }
 
 Page({
@@ -15,7 +84,10 @@ Page({
    */
   data: {
     com: {
-      d1: { id: com[2].id, title: com[2].name, sub: '', code: code.d1 }
+      d1: { id: com[2].id, title: com[2].name, sub: '', code: code.d1 },
+      d2: { id: com[9].id, title: com[9].name, sub: '中间三行', code: code.d2 },
+      d3: { id: com[9].id, title: com[9].name, sub: '中间一行', code: code.d3 },
+      d4: { id: com[9].id, title: com[9].name, sub: '中间两行', code: code.d4 }
     }
   },
 
