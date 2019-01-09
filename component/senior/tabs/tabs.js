7f7e6aecf4813973fa2f79fd24d366c6c39aa76f
qq865738120 <zwj865738120@163.com>
2019-01-09 19:00:19 +0800

    tabs组件开发

--- component/senior/tabs/tabs.js
+++ component/senior/tabs/tabs.js
@@ -8,19 +8,31 @@ Component({
       type: Array,
       value: ['item1', 'item2', 'item3', 'item3', 'item3', 'item3', 'item3', 'item3', 'item3'],
       observer: function (newVal) {
-        if (newVal) {
-
+        if (newVal && newVal.length < 5) {
+          console.log('newVal',newVal)
+          this.setData({
+            itemWidth: 750 / newVal.length
+          })
         }
       }
     },
+    height: {
+      type: String,
+      value: '120'
+    },
+    selectColor: {
+      type: String,
+      value: '#FE9036'
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    itemWidth: 33.3,
+    itemWidth: 187.5,
     isScroll: true,
+    scrollStyle: 'border-bottom: 1px solid #e5e5e5;'
   },
 
   /**
